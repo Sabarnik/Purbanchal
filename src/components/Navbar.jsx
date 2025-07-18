e3c7d57532fbd26aa722c9f7d6c0689d00cddfd7
Sabarnik <lahiri.sabarnik47@gmail.com>
2025-07-18 12:26:13 +0530

    Added Base Path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = () => {
     >
       <div className="max-w-7xl mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8 h-16 md:h-20">
         {/* Logo */}
-        <img src="/logo.png" alt="Logo" className="h-10 w-auto" />
+        <img src="./logo.png" alt="Logo" className="h-10 w-auto" />
 
         {/* Desktop Menu */}
         <div className="hidden md:flex items-center space-x-8">
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,12 @@
 // })
 
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  base: "./", 
+  plugins: [react(), tailwindcss()],
 });
+
+
